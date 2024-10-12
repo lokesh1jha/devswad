0c963120a098d3b0bd1b8ec476b7977be43bb2f3
lokeshjha <lokesh1jha@gmai.com>
2024-10-12 14:04:51 +0530

    layout fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,30 +28,28 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-gray-200 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <a href="/" className="text-[#4A4A4A]">DevSwaD</a>
-                    <div className="hidden md:ml-6 md:flex md:space-x-8">
-                      <NavLink href="/products">Our Products</NavLink>
-                      <NavLink href="/about">About Us</NavLink>
-                      <NavLink href="/contact">Contact Us</NavLink>
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-4">
-                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                    <MobileNavbar />
+          <div className="min-h-screen flex flex-col">
+            <nav className="w-full flex justify-center border-b border-b-gray-200 h-16">
+              <div className="w-full flex justify-between items-center p-3 px-4 sm:px-6 lg:px-8 text-sm">
+                <div className="flex gap-5 items-center font-semibold">
+                  <a href="/" className="text-[#4A4A4A]">DevSwaD</a>
+                  <div className="hidden md:ml-6 md:flex md:space-x-8">
+                    <NavLink href="/products">Our Products</NavLink>
+                    <NavLink href="/about">About Us</NavLink>
+                    <NavLink href="/contact">Contact Us</NavLink>
                   </div>
                 </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5 w-full">
-                {children}
+                <div className="flex items-center gap-4">
+                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <MobileNavbar />
+                </div>
               </div>
-              <Footer />
-            </div>
-          </main>
+            </nav>
+            <main className="flex-grow w-full">
+              {children}
+            </main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
